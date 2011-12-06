84826818ccce4264fb47cc5cf786a9a15bc83211
mdl <mr.dead.locked@gmail.com>
2011-12-06 14:44:54 -0500

    Trying to fix wifi

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,11 +26,11 @@ BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wext
 BOARD_HOSTAPD_DRIVER        := WEXT
 BOARD_HOSTAPD_PRIVATE_LIB   := lib_driver_cmd_wext
 BOARD_WLAN_DEVICE           := bcm4329
-WIFI_DRIVER_MODULE_PATH     := "/system/lib/modules/bcm4329.ko"
-WIFI_DRIVER_FW_PATH_STA     := "/vendor/firmware/fw_bcm4329.bin"
-WIFI_DRIVER_FW_PATH_AP      := "/vendor/firmware/fw_bcm4329_apsta.bin"
+WIFI_DRIVER_MODULE_PATH     := "/system/lib/hw/wlan/bcm4329.ko"
+WIFI_DRIVER_FW_PATH_STA     := "/system/lib/hw/wlan/fw_bcm4329.bin"
+WIFI_DRIVER_FW_PATH_AP      := "/system/lib/hw/wlan/fw_bcm4329_apsta.bin"
 WIFI_DRIVER_MODULE_NAME     := "bcm4329"
-WIFI_DRIVER_MODULE_ARG      := "firmware_path=/vendor/firmware/fw_bcm4329.bin nvram_path=/system/etc/nvram.txt iface_name=wlan0"
+WIFI_DRIVER_MODULE_ARG      := "firmware_path=/system/lib/hw/wlan/fw_bcm4329.bin nvram_path=/system/lib/hw/wlan/nvram.txt iface_name=wlan0"
 
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_CSR := true
