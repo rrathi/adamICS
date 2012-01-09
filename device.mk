3d72b8dca195a17b0fd784a1cc2d9348426d37a0
mrdeadlocked <mr.dead.locked@gmail.com>
2012-01-08 23:23:04 -0500

    misc changes and new deadcreme make/vendor setup changes

--- device.mk
+++ device.mk
@@ -127,7 +127,7 @@ PRODUCT_TAGS += dalvik.gc.type-precise
 
 PRODUCT_PACKAGES += \
 	librs_jni \
-	com.android.future.usb.accessory
+	com.android.future.usb.accessory 
 
 # Filesystem management tools
 PRODUCT_PACKAGES += \
