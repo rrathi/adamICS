20df7d2886f5687e5b73c1c2b093b7b18d8e3a49
Borkata <bvpopov@gmail.com>
2012-01-16 09:57:45 +0200

    We need to include GSM common stuff in order to enable Mobile Network menu in settings.

--- cm.mk
+++ cm.mk
@@ -6,6 +6,9 @@ $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 # Inherit device configuration.
 $(call inherit-product, device/notionink/adam/full_adam.mk)
 
+# Inhert GSM common stuff
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
 # Change these to values from a stock ADAM rom
 #PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=EeePad BUILD_ID=HTK75 BUILD_DISPLAY_ID=HTK75 BUILD_FINGERPRINT="asus/WW_epad/EeePad:3.2.1/HTK75/WW_epad-8.6.5.18-20111028:user/release-keys" PRVIATE_BUILD_DESC="WW_epad-user 3.2.1 HTK75 WW_epad-8.6.5.18-20111028 release-keys"
 
